1623914b3afbc81bf5938c0152a493eb41167e1e
Marco Echevestre <devminotto@gmail.com>
2021-08-03 13:52:14 +0200

    Implement global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import Head from 'next/head';
-import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Ignews</title>
+        <title>Home | Ignews</title>
       </Head>
-      <h1 className={styles.title}></h1>
+      <h1>Ignews</h1>
     </>
   );
 }
