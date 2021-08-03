58fa751e6621fbb3e2842d5acb2863cae946471a
Marco Echevestre <devminotto@gmail.com>
2021-08-03 13:19:21 +0200

    Install and implement sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import styles from '../styles/home.module.scss';
+
 export default function Home() {
   return (
-    <h1>Ignews</h1>
+    <h1 className={styles.title}>Ignews</h1>
    )
 }
