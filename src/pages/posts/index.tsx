da4053a04a3d79ef7492b0608ae6a5fb861a9adb
Marco Echevestre <devminotto@gmail.com>
2021-08-16 09:57:37 +0200

    List posts on screen

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,7 +1,23 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+};
+
+interface PostsProps {
+  posts: Post[];
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -10,44 +26,52 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <title>12 de março de 2021</title>
-            <strong>
-              Next.JS - Novidades na versão 10 e atualização do blog para
-              melhorar a performance
-            </strong>
-            <p>
-              Se você nos acompanhou nos últimos posts, já viu que criamos um
-              blog com um contador de visitas usando o MongoDB e Next.js, depois
-              adicionamos a funcionalidade de dark mode.
-            </p>
-          </a>
-          <a href="#">
-            <title>12 de março de 2021</title>
-            <strong>
-              Next.JS - Novidades na versão 10 e atualização do blog para
-              melhorar a performance
-            </strong>
-            <p>
-              Se você nos acompanhou nos últimos posts, já viu que criamos um
-              blog com um contador de visitas usando o MongoDB e Next.js, depois
-              adicionamos a funcionalidade de dark mode.
-            </p>
-          </a>
-          <a href="#">
-            <title>12 de março de 2021</title>
-            <strong>
-              Next.JS - Novidades na versão 10 e atualização do blog para
-              melhorar a performance
-            </strong>
-            <p>
-              Se você nos acompanhou nos últimos posts, já viu que criamos um
-              blog com um contador de visitas usando o MongoDB e Next.js, depois
-              adicionamos a funcionalidade de dark mode.
-            </p>
-          </a>
+          {posts.map((post) => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.content'],
+      pageSize: 100,
+    }
+  );
+
+  const posts = response.results.map((post) => {
+    console.log(post.data.content);
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt:
+        post.data.content.find((content) => content.type === 'paragraph')
+          ?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
